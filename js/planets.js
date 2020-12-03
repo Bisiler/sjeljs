584310a3e920a61115a4ca3ef703ffbd1b3fe56a
Opilane TTHK <opilane@tthk.ee>
2020-12-03 09:25:37 +0200

    create web page

---
+++ js/planets.js
@@ -0,0 +1,33 @@
+let planets = ["Mercury", "venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+console.log(planets[5]);
+console.log(planets[6]);
+console.log(planets.length)
+for(let i = 0; i < 7; i++){
+    let message = `${i}. ${planets[i]}`;
+    console.log(message);
+}
+planets.push("Pluto");
+console.log(planets);
+
+const html = `
+<ul>
+<li>${planets[0]}</li>
+<li>${planets[1]}</li>
+<li>${planets[2]}</li>
+<li>${planets[3]}</li>
+<li>${planets[4]}</li>
+<li>${planets[5]}</li>
+<li>${planets[6]}</li>
+<ul/>
+
+`;
+document.body.innerHTML = html;
+
+
+
+
